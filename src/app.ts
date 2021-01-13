21f0f59deee77339f1b4a8f837809a80aa008a02
Adam Wasilewski <brzoza10k@gmail.com>
2021-01-13 19:23:39 +0100

    Add env variables

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 
 const app = express();
-const port = process.env.SERVER_PORT || 3000;
+const port = process.env.SERVER_PORT;
 
 app.listen(port, () => {
   console.log(`Server is up on port ${port}`);
