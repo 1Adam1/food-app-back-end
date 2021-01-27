1cad80c92d0c0599789f23f59eabf85a99072d14
Adam Wasilewski <brzoza10k@gmail.com>
2021-01-27 23:31:16 +0100

    Implement rest of basic CRUD operations

--- src/api/shopping-list.api.ts
+++ src/api/shopping-list.api.ts
@@ -1,4 +1,5 @@
 import { Router, Response } from "express";
+import { ShoppingListDataModelIdexableInterface } from "../database/interfaces/shopping-list.model.interface";
 import ShoppingList from "../database/models/shopping-list";
 import { ExtendedRequestType } from "../database/types/extended-requests.type";
 import { AuthenticationService } from "../services/authentication/authentication.service";
@@ -24,6 +25,56 @@ router.post('/shopping-lists',
       response.status(201).send(shoppingList.toJSON());
     } catch (error) {
       response.status(400).send(error);
+    }
+  }
+);
+
+router.get('/shopping-lists/:shoppingListId',
+  AuthenticationService.authenticateUser,
+  ShoppingListAuthorizationService.autorizateShoppingList,
+  async (request: ExtendedRequestType, response: Response) => {
+    try {
+      response.send(request.extendedData!.shoppingList);
+    } catch (error) {
+      response.status(400).send(error);
+    }
+  }
+);
+
+router.patch('/shopping-lists/:shoppingListId',
+  AuthenticationService.authenticateUser,
+  ShoppingListAuthorizationService.autorizateShoppingList,
+  ShoppingListAuthorizationService.authorizeShoppingListsItems,
+  async (request: ExtendedRequestType, response: Response) => {
+    const allowedFieldsKeys = ['date', 'description', 'items'];
+    const bodyFieldKeys = Object.keys(request.body);
+    const operationIsValid = bodyFieldKeys.every(key => allowedFieldsKeys.includes(key));
+  
+    if (!operationIsValid) {
+      return response.status(400).send({error: 'Invalid updates'});
+    }
+
+    try {
+      bodyFieldKeys.forEach(key => (request.extendedData!.shoppingList as ShoppingListDataModelIdexableInterface)[key] = request.body[key]);
+      request.extendedData!.shoppingList!.totalPrice = AmountService.countTotalPrice(request.extendedData!.shoppingListItems!);
+      await request.extendedData!.shoppingList!.save();
+  
+      response.send(request.extendedData!.shoppingList!);
+    } catch (error) {
+      response.status(400).send();
+    }
+  }
+);
+
+router.delete('/shopping-lists/:shoppingListId',
+  AuthenticationService.authenticateUser,
+  ShoppingListAuthorizationService.autorizateShoppingList,
+  async (request: ExtendedRequestType, response: Response) => {
+    try {
+      await request.extendedData!.shoppingList!.remove();
+      response.send();
+    } catch (error) {
+      response.status(500).send();
     }
   }
 );
